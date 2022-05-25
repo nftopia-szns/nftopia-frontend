fbdaf324e70fd9347ed539f59cf55be9ed0a7830
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-05-25 04:58:42 +0000

    add docker file

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
   env: {
       NEXT_SERVER_PORT: process.env.NEXT_SERVER_PORT,
       NEXT_SERVER_HOST: process.env.NEXT_SERVER_HOST,
-  }
+  },
+  experimental: {
+    outputStandalone: true,
+  },
 }
 
 module.exports = nextConfig
