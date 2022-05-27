d89558d7de2fe7e778ac3da4a10e67e57bd13382
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-05-27 18:29:59 +0000

    chores: use header of antd

--- components/layout/NFTopiaLayout.tsx
+++ components/layout/NFTopiaLayout.tsx
@@ -1,14 +1,98 @@
-import { ReactNode, useState } from "react"
-import { NFTopiaHeader } from "."
+import { useWeb3React } from "@web3-react/core"
+import { Button, Dropdown, Layout, Menu, Modal, Space } from "antd"
+import { Header, Content, Footer } from "antd/lib/layout/layout"
+import { ReactNode, useEffect, useState } from "react"
+import CoinbaseWalletCard from "../connect-wallet/connectorCards/CoinbaseWalletCard"
+import MetaMaskCard from "../connect-wallet/connectorCards/MetaMaskCard"
+import WalletConnectCard from "../connect-wallet/connectorCards/WalletConnectCard"
+import { shortenAddress } from "../../utils/eth";
+import { DownOutlined } from '@ant-design/icons';
+
 import "./NFTopiaLayout.module.css"
+import { MenuInfo } from "rc-menu/lib/interface"
 
 const NFTopiaLayout = ({ children }: { children: ReactNode }) => {
+    const { account, connector } = useWeb3React()
+    const [showConnectWalletModal, setShowConnectWalletModal] = useState(false)
+
+    useEffect(() => {
+        connector.connectEagerly()
+    }, [])
+
+    const onDisconnect = (info: MenuInfo) => {
+        connector.deactivate()
+    }
+
     return (
         <>
-            <NFTopiaHeader />
-            <div className="site-layout-content" style={{ padding: '100 50px' }}>
-                {children}
-            </div>
+            <Layout className="layout">
+                <Header>
+                    {/* <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['1']}
+                        items={new Array(3).fill(null).map((_, index) => {
+                            const key = index + 1;
+                            return {
+                                key,
+                                label: `nav ${key}`,
+                            };
+                        })}
+                    /> */}
+                    {account ?
+                        <Dropdown
+                            className="dropdown-btn"
+                            trigger={['click']}
+                            overlay={
+                                <Menu
+                                    items={[
+                                        {
+                                            label: 'Disconnect',
+                                            key: 'disconnect',
+                                            onClick: (_) => { connector.deactivate() }
+                                        },
+                                    ]}
+                                />
+                            }>
+                            <a onClick={e => e.preventDefault()}>
+                                <Space>
+                                    {shortenAddress(account)}
+                                    <DownOutlined />
+                                </Space>
+                            </a>
+                        </Dropdown>
+                        :
+                        <>
+                            <Button
+                                key="connect-wallet"
+                                onClick={() => { setShowConnectWalletModal(true) }}
+                            >
+                                Connect Wallet
+                            </Button>
+                            <Modal
+                                closable={false}
+                                maskClosable={true}
+                                footer={null}
+                                visible={showConnectWalletModal}
+                                onCancel={() => setShowConnectWalletModal(false)}
+                            >
+                                <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
+                                    <MetaMaskCard />
+                                    <WalletConnectCard />
+                                    <CoinbaseWalletCard />
+                                </div>
+                            </Modal>
+                        </>
+                    }
+                </Header>
+                <Content style={{ padding: '100 50px' }}>
+                    <div className="site-layout-content" style={{ padding: '100 50px' }}>
+                        {children}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+
         </>
     )
 }
