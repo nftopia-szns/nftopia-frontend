b2ef1ca0234deeb0596b5773f41c3b311b39d43a
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-07-07 03:46:30 +0000

    update generic asset dto

--- services/search/search.saga.ts
+++ services/search/search.saga.ts
@@ -1,13 +1,14 @@
 import { PayloadAction } from "@reduxjs/toolkit"
 import { searchStart, searchSuccess } from './search-slice';
 import { put, select, takeLatest } from 'redux-saga/effects';
-import { DecentralandSearchHitDto, SearchDto } from "../../pages/api/search/search.types";
+import { SearchDto } from "../../pages/api/search/search.types";
 import { enhancedSearch } from "../../pages/api/search";
+import { GenericAssetDto } from "nftopia-shared/dist/shared/asset/types";
 
 export function* search(action: PayloadAction<SearchDto>) {
     try {
         const searchDto = action.payload
-        const _searchResults = yield enhancedSearch<DecentralandSearchHitDto>(searchDto)
+        const _searchResults = yield enhancedSearch<GenericAssetDto>(searchDto)
 
         // dispatch action from saga
         yield put(searchSuccess(_searchResults))
